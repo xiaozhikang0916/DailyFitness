e1873ead37a4971e73ec8d9135c66f2b35d93424
xiaozhikang <xiaozhikang0916@gmail.com>
2026-09-13 10:33:50 +0800

    Update version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "site.xiaozk.dailyfitness"
         minSdk = 29
         targetSdk = 36
-        versionCode = 102000
-        versionName = "1.2.0"
+        versionCode = 103000
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
